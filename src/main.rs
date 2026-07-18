d9109ab602d8e6354a5cb8628a82b1f6a0ac7fd5
asd <aravindhv10@gmail.com>
2026-07-18 14:34:04 +0530

    Added more dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 #[compio::main]
 async fn main() -> std::io::Result<()> {
-    println!("Hello, world!");
+    let tmp: Vec<String> = std::env::args().collect();
+    for i in 1..tmp.len() {
+    let res = compio::fs::read("./main.rs").await?;
+    }
+
+    eprintln!("{:?}", res);
+
     Ok(())
 }
