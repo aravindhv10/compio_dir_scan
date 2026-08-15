36d99e32ffb1556e6e95306724d8ad26acf787ae
asd <aravindhv10@gmail.com>
2026-08-15 12:01:33 +0530

    More updates

--- src/main.rs
+++ src/main.rs
@@ -65,29 +65,28 @@ impl VideoReader {
         let opt = memfd::MemfdOptions::default();
         let pid = rustix::process::getpid();
 
-        let out_memfd = memfd::MemfdOptions::default().create(name.to_string() + "out")?;
+        let out_memfd = opt.create(name.to_string() + "out")?;
         let out_fd = out_memfd.as_raw_fd();
         let out_fdpath = format!("/proc/{}/fd/{}", pid, out_fd);
 
-        let res = std::process::Command::new("ffmpeg")
-            .arg("-y")
-            .arg("-i")
-            .arg(fdpath.as_ref())
-            .arg("-vf")
-            .arg("fps=8,scale=1280:720")
-            .arg("-nostdin")
-            .arg("-loglevel")
-            .arg("quiet")
-            .arg("-f")
-            .arg("rawvideo")
-            .arg("-pix_fmt")
-            .arg("rgb24")
-            .arg(&out_fdpath)
-            .status()?;
-
-        let memmap = unsafe { memmapix::Mmap::map(out_memfd.as_file()) }?;
-
-        Ok(Self { data: memmap })
+        match ffmpeg_video_to_raw(
+            /*path_in: impl AsRef<std::path::Path> =*/ fdpath,
+            /*path_out: impl AsRef<std::path::Path> =*/ out_fdpath,
+        )?
+        .code()
+        {
+            Some(i) => {
+                if i != 0 {
+                    return Err(anyhow::format_err!("ffmpeg failed with error code {}", i));
+                } else {
+                    let memmap = unsafe { memmapix::Mmap::map(out_memfd.as_file()) }?;
+                    return Ok(Self { data: memmap });
+                }
+            }
+            None => {
+                return Err(anyhow::format_err!("ffmpeg failed with unknown error code"));
+            }
+        };
     }
 }
 
