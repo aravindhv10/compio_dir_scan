f287dc4190df6b04f442b1219708c0f4ccf30a95
asd <aravindhv10@gmail.com>
2026-08-15 11:59:13 +0530

    More refinements

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,27 @@
 use std::{io::Write, os::fd::AsRawFd};
 
+fn ffmpeg_video_to_raw(
+    path_in: impl AsRef<std::path::Path>,
+    path_out: impl AsRef<std::path::Path>,
+) -> anyhow::Result<std::process::ExitStatus> {
+    let res = std::process::Command::new("ffmpeg")
+        .arg("-y")
+        .arg("-i")
+        .arg(path_in.as_ref())
+        .arg("-vf")
+        .arg("fps=8,scale=1280:720")
+        .arg("-nostdin")
+        .arg("-loglevel")
+        .arg("quiet")
+        .arg("-f")
+        .arg("rawvideo")
+        .arg("-pix_fmt")
+        .arg("rgb24")
+        .arg(path_out.as_ref())
+        .status()?;
+    Ok(res)
+}
+
 struct VideoReader {
     data: memmapix::Mmap,
 }
@@ -13,32 +35,30 @@ impl VideoReader {
         let fd = memfd.as_raw_fd();
         let fdpath = format!("/proc/{}/fd/{}", pid, fd);
 
-        memfd.as_file().write_all(indata);
-        drop(indata);
-
-        let out_memfd = memfd::MemfdOptions::default().create(name.to_string() + "out")?;
+        let out_memfd = opt.create(name.to_string() + "out")?;
         let out_fd = out_memfd.as_raw_fd();
         let out_fdpath = format!("/proc/{}/fd/{}", pid, out_fd);
 
-        let res = std::process::Command::new("ffmpeg")
-            .arg("-y")
-            .arg("-i")
-            .arg(fdpath)
-            .arg("-vf")
-            .arg("fps=8,scale=1280:720")
-            .arg("-nostdin")
-            .arg("-loglevel")
-            .arg("quiet")
-            .arg("-f")
-            .arg("rawvideo")
-            .arg("-pix_fmt")
-            .arg("rgb24")
-            .arg(&out_fdpath)
-            .status()?;
+        let _ = memfd.as_file().write_all(indata)?;
 
-        let memmap = unsafe { memmapix::Mmap::map(out_memfd.as_file()) }?;
-
-        Ok(Self { data: memmap })
+        match ffmpeg_video_to_raw(
+            /*path_in: impl AsRef<std::path::Path> =*/ fdpath,
+            /*path_out: impl AsRef<std::path::Path> =*/ out_fdpath,
+        )?
+        .code()
+        {
+            Some(i) => {
+                if i != 0 {
+                    return Err(anyhow::format_err!("ffmpeg failed with error code {}", i));
+                } else {
+                    let memmap = unsafe { memmapix::Mmap::map(out_memfd.as_file()) }?;
+                    return Ok(Self { data: memmap });
+                }
+            }
+            None => {
+                return Err(anyhow::format_err!("ffmpeg failed with unknown error code"));
+            }
+        };
     }
 
     fn from_path(fdpath: impl AsRef<std::path::Path>, name: &str) -> anyhow::Result<Self> {
